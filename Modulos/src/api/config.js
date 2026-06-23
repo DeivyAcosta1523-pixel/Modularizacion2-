1223d1e9519b4bb13cd67ab430ad84120e133a6c
santiago carrillo rivera <carrilloriverasantiago@gmail.com>
2026-06-23 13:52:14 -0500

    realice cambios porque la ip sigue sin funcionar

--- Modulos/src/api/config.js
+++ Modulos/src/api/config.js
@@ -1,1 +1,1 @@
-export const API_URL = window.location.origin;
+export const API_URL = 'http://10.0.101.248:3001';
--- Modulos/vite.config.js
+++ Modulos/vite.config.js
@@ -4,8 +4,8 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     proxy: {
-      '/users': 'http://localhost:3001',
-      '/tareas': 'http://localhost:3001'
+      '/users': 'http:// 172.25.208.1:3001',
+      '/tareas': 'http:// 172.25.208.1:3001'
     }
   }
 })
